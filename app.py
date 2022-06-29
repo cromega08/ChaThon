18cfd9e352edee0c02564e9fbc3a8c3970b3896f
cromega <cr.jrg08@gmail.com>
2022-06-29 15:37:39 -0500

    functional

--- app.py
+++ app.py
@@ -2,51 +2,58 @@ from server import stream
 import argparse
 import re
 
-parser = argparse.Argumentparser(prog="Chyt",
+parser = argparse.ArgumentParser(prog="ChaThon",
                                 description="A CLI Chat Application with per to per connecctions",
-                                usage="One of the users need to init the connection, after oter people could connect",
+                                usage="ChaThon start_as [-h] [-n 1] [-ho 123.456.7.89] [-p 0000]",
                                 epilog="Connect with all yout friends!")
-
 parser.add_argument("create",
                     nargs=1,
                     choices=["c", "s", "client", "server"],
                     action="store",
                     type=str,
                     default="server",
-                    required=True,
-                    metavar="[client|server]",
-                    help="Set Chyt to act as server or client")
+                    metavar="start_as",
+                    help="Set ChaThon to act as server or client. Options are: 'c'|'s'|'client'|'server'")
 parser.add_argument("-n", "--num_clients",
                     nargs=1,
                     action="store",
                     type=int,
+                    default=-1,
                     metavar="1",
                     help="port number opened by the server(required if started a server)")
-parser.add_argument("-h", "--host",
+parser.add_argument("-ip", "--host",
                     nargs=1,
                     action="store",
                     type=str,
+                    default="",
                     metavar="123.456.7.89",
                     help="IP address of the host (required if started a client)")
 parser.add_argument("-p", "--port",
                     nargs=1,
                     action="store",
                     type=int,
+                    default=-1,
                     metavar="0000",
                     help="port number opened by the server(required if started a client)")
 
 arguments = parser.parse_args()
 
-match arguments.create:
+print(arguments.host, arguments.port, arguments.num_clients)
+
+match arguments.create[0]:
 
     case ("c"|"create"):
 
-        validation = [re.search("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}", arguments.host) != None, 0 < arguments.port < 65536]
+        validation = [re.search("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}", arguments.host[0]) != None, 0 < arguments.port[0] < 65536, arguments.num_clients < 0]
 
-        if all(validation): stream(arguments.host, arguments.port).create_client()
+        if all(validation): stream(arguments.host[0], arguments.port[0]).create_client()
+        else: print("Incorrect use of client option, use \"-h\" to get help")
 
     case ("s"|"server"):
 
-        if arguments.num_clients > 0: stream().create_server()
+        validation = [arguments.host == "", arguments.port == -1, arguments.num_clients[0] > 0]
+
+        if all(validation): stream().create_server(arguments.num_clients[0])
+        else: print("Incorrect use of server option, use \"-h\" to get help")
 
     case _: print("Incorrect parameters, use \"-h\" to get help")
