be30075fa30ed64b518ab22ac15a77d3cc7bbff2
cromega <cr.jrg08@gmail.com>
2022-06-30 15:45:44 -0500

    IT'S ALIVE!

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from _thread import *
-import asyncio
 from select import select
 import socket
 import subprocess
@@ -7,100 +6,129 @@ import sys
 import random
 
 class stream:
+    
     def __init__(self, host = socket.gethostbyname(socket.gethostname()), port = None) -> None:
+        
         try:
+            
             self.stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.host = host
             self.port = port if port != None else self.get_avaible()
-            self.i = False
-            self.clients = []
+        
         except: print("An error ocurred with socket creation")
 
     def create_server(self, num_clients: int) -> None:
+        
         try:
+
             print(self.host, self.port, sep="\n", end="\n")
+
             self.stream.bind((self.host, self.port))
             self.stream.listen(int(num_clients) + 1)
+            self.i = False
             self_created = False
+            self.clients = []
+            self.close = False
+            
             while True:
+
+                if self.close: break
+
                 connection, client = self.stream.accept()
                 self.clients.append(connection)
                 start_new_thread(self.each_client, (connection, client[0]))
                 self.send_message(bytes(f"[{client[0]}] connected", "utf-8"), connection)
-                if not self_created: start_new_thread(self.self_client, ())
-                self_created = True
-                if self.clients == 0: break
+                
+                if not self_created: start_new_thread(self.self_client, ()); self_created = True
+        
         finally:
-            connection.close()
+            
             self.stream.close()
 
     def each_client(self, connection:socket.socket, client:str) -> None:
 
-        connection.sendall(bytes(f"Your connection with: {socket.gethostbyname(socket.gethostname())} was successful", "utf-8"))
+        connection.sendall(bytes(f"\nYour connection with: {socket.gethostbyname(socket.gethostname())} was successful", "utf-8"))
         
         while True:
+            
             try:
+                
                 message = connection.recv(2022)
-                print("here")
+                
                 if message != b"":
-                    print("received")
-                    to_send = f"[{client}]: {message}"
-                    self.send_message(bytes(message, "utf-8"), connection)
+                    
+                    to_send = f"[{client}]: {message.decode('utf-8')}"
+                    self.send_message(bytes(to_send, "utf-8"), connection)
+                
                 else: self.clients.remove(connection); connection.close(); break
+            
             except: continue
-    
+
+        if len(self.clients) == 0: self.kill()
+
     def send_message(self, message:bytes, connection:socket.socket) -> None:
+        
         for client in self.clients:
+            
             if client != connection or self.i == True:
+                
                 try: client.sendall(message)
+                
                 except: client.close(); self.clients.remove(client)
 
     def create_client(self) -> None:
+
         try:
+
             self.stream.connect((self.host, self.port))
+
             while True:
+
                 sockets_list = [sys.stdin, self.stream]
                 read_sockets, write_sockets, error_sockets = select(sockets_list, [], [])
+                
                 for sockets in read_sockets:
+                    
                     if sockets == self.stream:
-                        print("received client")
+                        
                         message = sockets.recv(2022)
+                        
                         if message == b"": break
-                        print(message)
+                        
+                        print(f"{message.decode('utf-8')}\n")
+                    
                     else:
-                        message = input("[You]:")
+                        
+                        message = input("-> ")
                         self.stream.sendall(bytes(message, "utf-8"))
-                        print("\n")
+                
                 if message == b"": break
-        finally: self.stream.close(); print(f"\nConnection with {self.host} was finished"); sys.exit()
+
+        finally: self.stream.close(); print(f"Connection with {self.host} was finished"); sys.exit()
 
     def self_client(self) -> None:
+
         self.i = True
         subprocess.call(["xterm", "-e", f"python -u /home/cromega/programming/projects/cli_chat_application/app.py c -ip {self.host} -p {self.port}"])
-        # try:
-        #     self_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #     self_client.connect((self.host, self.port))
-        #     while True:
-        #         sockets_list = [sys.stdin, self_client] 
-        #         read_sockets, write_sockets, error_sockets = select(sockets_list, [], [])
-        #         for sockets in read_sockets:
-        #             if sockets == self_client: print(sockets.recv(2022))
-        #             else:
-        #                 message = input("[You]: ")
-        #                 self_client.sendall(bytes(message, "utf-8"))
-        #                 print("\n")
-        # finally: self_client.close()
         
     def get_avaible(self) -> int:
 
         port_n = random.randint(1, 65535)
+
         try:
+
             stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             stream.connect(("localhost", port_n))
             connected = True
             port_n += 1
-        except:
-            connected = False
+
+        except: connected = False
+        
         finally:
+            
             stream.close()
             return port_n if not connected else self.get_avaible()
+    
+    def kill(self) -> None:
+
+        self.close = True; self.self_client()
